4f94c25ec05a35669f9ea646843f98e56edd4bf7
Mayukh <mayukhdebgoswami82@gmail.com>
2025-07-08 15:42:04 +0530

    Footer is ready..

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className='bg-white py-6 text-center text-gray-600 shadow-lg border-t-[1px] border-gray-200'>
+      {/* --Footer Text-- */}
+      <h1>© 2025 TickTrack • All rights reserved • Made with ❤️ by Pirate</h1>
+    </div>
+  );
+};
+
+export default Footer;
