4db9f259e5a850120157420d43073424c5198ec1
Mayukh <mayukhdebgoswami82@gmail.com>
2025-11-26 00:56:30 +0530

    Authentication added in dashboard page...

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,16 +1,35 @@
+'use client';
+
 import DashboardPage from '@/components/Dashboard';
 import TimeAndDate from '@/ui/TimeAndDate';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import { useRouter } from 'next/navigation';
+import { getCookie } from 'cookies-next';
+import toast from 'react-hot-toast';
 
 const Dashboard = () => {
+  const router = useRouter();
+  const hasShownToast = useRef(false); // Prevent double toast
+
+  useEffect(() => {
+    const token = getCookie('user_token');
+
+    if (!token && !hasShownToast.current) {
+      hasShownToast.current = true; // block next render
+      toast.error('Please sign in first to visit the dashboard page.');
+      router.push('/');
+    }
+  }, [router]);
+
   return (
     <div>
       <Navbar />
+
       <div className='h-full py-6 px-4 md:py-10 md:px-32'>
         <div className='flex flex-col-reverse md:flex-row justify-between items-start md:items-center gap-6'>
-          {/* --Title and para -- */}
+          {/* Title */}
           <div>
             <h1 className='text-2xl md:text-3xl font-bold mb-2'>TickTrack</h1>
             <p className='text-gray-500 max-w-md'>
@@ -18,16 +37,18 @@ const Dashboard = () => {
             </p>
           </div>
 
-          {/* --Time and date-- */}
+          {/* Time and date */}
           <div className='text-right w-full md:w-auto'>
             <TimeAndDate />
           </div>
         </div>
-        {/* --Dashboard-- */}
+
+        {/* Dashboard Content */}
         <div className='mt-9'>
           <DashboardPage />
         </div>
       </div>
+
       <Footer />
     </div>
   );
